65e10502aebbb42e9131c12c196205c7a2be2073
KudryashovAlexander <sasha080193@gmail.com>
2023-05-09 09:38:14 +0300

    Настроен LaunchScreen, добавлен логотип и цвета из проекта

---
+++ ImageFeed/Helpers/UIColor+Extension.swift
@@ -0,0 +1,10 @@
+import UIKit
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "ypBlack") ?? UIColor.black }
+    static var ypGray: UIColor { UIColor(named: "ypGray") ?? UIColor.gray }
+    static var ypBackground: UIColor { UIColor(named:"ypBackground") ?? UIColor.gray }
+    static var ypWhite: UIColor { UIColor(named: "ypWhite") ?? UIColor.white }
+    static var ypWhiteAplpha: UIColor { UIColor(named: "ypWhiteAplpha") ?? UIColor.white }
+    static var ypRed: UIColor { UIColor(named: "ypRed") ?? UIColor.red }
+    static var ypBlue: UIColor { UIColor(named: "ypBlue") ?? UIColor.blue}
+}
