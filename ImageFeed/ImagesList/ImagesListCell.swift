96961c00462ac51d2ae82c6a0b21680352ae2109
KudryashovAlexander <sasha080193@gmail.com>
2023-05-13 12:18:39 +0300

    Исправлено по комментариям

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -1,28 +1,20 @@
 import UIKit
+
 final class ImagesListCell: UITableViewCell {
     
     //MARK: - Outlets
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var dateLabel: UILabel!
-    
     @IBOutlet weak var gradientImageView: UIImageView!
     
     static let reuseIdentifier = "ImagesListCell"
     
-    func gradient() {
-        let color1 = UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 0).cgColor
-        let color2 = UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 0.2).cgColor
-        let gradientLayer = CAGradientLayer()
+    //MARK: - Methods
+    func createCornerRadiusGradient() {
         gradientImageView.clipsToBounds = true
         gradientImageView.layer.cornerRadius = 16
         gradientImageView.layer.maskedCorners = [.layerMaxXMaxYCorner,.layerMinXMaxYCorner]
-        gradientLayer.frame = gradientImageView.bounds
-        gradientLayer.colors = [color1, color2]
-        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
-        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
-        gradientImageView.layer.insertSublayer(gradientLayer, at: 0)
     }
-    
     
 }
